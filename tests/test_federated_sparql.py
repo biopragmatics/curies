becd230569f4b56be3e9b57be8fd1896c0e7f259
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-03-24 16:42:11 +0100

    Update test_federated_sparql.py

--- tests/test_federated_sparql.py
+++ tests/test_federated_sparql.py
@@ -5,11 +5,11 @@
 TODO: we might want to add checks if the endpoints are up, and skip the test if not up
 """
 
-import os
 import subprocess
 import time
 import unittest
 from multiprocessing import Process
+from textwrap import dedent
 
 import pystow
 import requests
@@ -19,82 +19,27 @@ from curies import Converter
 from curies.mapping_service import get_fastapi_mapping_app
 from tests.test_mapping_service import PREFIX_MAP
 
-BIOREGISTRY_SPARQL_ENDPOINT = "https://bioregistry.io/sparql"
+BIOREGISTRY_SPARQL_ENDPOINT = "http://bioregistry.io/sparql"
 TEST_QUERY = 'SELECT ?test WHERE { BIND("hello" as ?test) }'
 
 
-class TestPublicFederatedSparql(unittest.TestCase):
-    """Test the identifier mapping service."""
+class FederationMixin(unittest.TestCase):
+    """A shared mixin for testing."""
 
-    def setUp(self) -> None:
-        self.sparql = f"""\
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        SELECT DISTINCT ?o WHERE {{
-            SERVICE <{BIOREGISTRY_SPARQL_ENDPOINT}> {{
-                <http://purl.obolibrary.org/obo/CHEBI_24867> owl:sameAs ?o
-            }}
-        }}
-        """.rstrip()
-
-    def query_endpoint(self, endpoint: str, query: str):
-        """Query an endpoint."""
-        # TODO assert that the endpoint is up and running before
-        #  trying to test the service
-        try:
-            resp = requests.get(
-                endpoint,
-                params={"query": query},
-                headers={"accept": "application/json"},
-            )
-            res = resp.json()
-            self.assertGreater(
-                len(res["results"]["bindings"]),
-                0,
-                msg=f"Federated query to {endpoint} gives no results",
-            )
-            return res["results"]["bindings"]
-        except Exception:
-            self.fail(msg=f"Error running the federated query to {endpoint}: {resp.text}")
-
-    def test_public_federated_virtuoso(self):
-        """Test sending a federated query to a public mapping service from Virtuoso."""
-        self.query_endpoint("https://bio2rdf.org/sparql", self.sparql)
-
-    def test_public_federated_blazegraph(self):
-        """Test sending a federated query to a public mapping service from Blazegraph"""
-        self.query_endpoint("http://kg-hub-rdf.berkeleybop.io/blazegraph/sparql", self.sparql)
-
-    def test_public_federated_graphdb(self):
-        """Test sending a federated query to a public mapping service from GraphDB."""
-        self.query_endpoint("https://graphdb.dumontierlab.com/repositories/test", self.sparql)
-
-
-def _get_app():
-    converter = Converter.from_priority_prefix_map(PREFIX_MAP)
-    app = get_fastapi_mapping_app(converter)
-    return app
-
-
-class TestFederatedSparql(unittest.TestCase):
-    """Test the identifier mapping service."""
-
-    proc: Process
-    proc_blazegraph: Process
-
-    def get(self, sparql: str, accept: str = "application/json") -> requests.Response:
+    def get(
+        self, endpoint: str, sparql: str, *, accept: str = "application/json"
+    ) -> requests.Response:
         """Get a response from a given SPARQL query."""
-        return requests.get(
-            self.blazegraph_endpoint,
+        response = requests.get(
+            endpoint,
             params={"query": sparql},
             headers={"accept": accept},
         )
+        return response
 
-    def assert_blazegraph_running(self):
-        """Check that blazegraph is running properly."""
-        try:
-            res = self.get(TEST_QUERY)
-        except requests.exceptions.ConnectionError:
-            return self.fail("blazegraph is not running")
+    def assert_service_works(self, endpoint: str):
+        """Assert that a service is able to accept a simple SPARQL query."""
+        res = self.get(endpoint, TEST_QUERY, accept="application/json")
         res_json = res.json()
         self.assertIn("results", res_json)
         self.assertIn("bindings", res_json["results"])
@@ -103,6 +48,80 @@ class TestFederatedSparql(unittest.TestCase):
         self.assertIn("value", res_json["results"]["bindings"][0]["test"])
         self.assertEqual("hello", res_json["results"]["bindings"][0]["test"]["value"])
 
+
+class TestPublicFederatedSPARQL(FederationMixin):
+    """Test the identifier mapping service."""
+
+    def setUp(self) -> None:
+        """Set up the public federated SPARQL test case."""
+        self.sparql = dedent(
+            f"""\
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        SELECT DISTINCT ?o WHERE {{
+            SERVICE <{BIOREGISTRY_SPARQL_ENDPOINT}> {{
+                <http://purl.obolibrary.org/obo/CHEBI_24867> owl:sameAs ?o
+            }}
+        }}
+        """.rstrip()
+        )
+
+    def query_endpoint(self, endpoint: str):
+        """Query an endpoint."""
+        self.assert_service_works(endpoint)
+
+        accept = "application/sparql-results+json"
+        resp = self.get(endpoint, self.sparql, accept=accept)
+        self.assertEqual(
+            200,
+            resp.status_code,
+            msg=f"SPARQL query failed at {endpoint}:\n\n{self.sparql}\n\nResponse:\n{resp.text}",
+        )
+        response_content_type = resp.headers["content-type"].split(";")[0]
+        self.assertEqual(accept, response_content_type, msg="Server sent incorrect content type")
+
+        try:
+            res = resp.json()
+        except Exception:
+            self.fail(msg=f"\n\nError running the federated query to {endpoint}:\n{resp.text}")
+        self.assertGreater(
+            len(res["results"]["bindings"]),
+            0,
+            msg=f"Federated query to {endpoint} gives no results",
+        )
+        self.assertIn(
+            "https://bioregistry.io/chebi:24867",
+            {binding["o"]["value"] for binding in res["results"]["bindings"]},
+        )
+
+    def test_public_federated_virtuoso(self):
+        """Test sending a federated query to a public mapping service from Virtuoso."""
+        self.query_endpoint("https://bio2rdf.org/sparql")
+
+    def test_public_federated_blazegraph(self):
+        """Test sending a federated query to a public mapping service from Blazegraph."""
+        self.query_endpoint("http://kg-hub-rdf.berkeleybop.io/blazegraph/sparql")
+
+    def test_public_federated_graphdb(self):
+        """Test sending a federated query to a public mapping service from GraphDB."""
+        self.query_endpoint("https://graphdb.dumontierlab.com/repositories/test")
+
+
+def _get_app():
+    converter = Converter.from_priority_prefix_map(PREFIX_MAP)
+    app = get_fastapi_mapping_app(converter)
+    return app
+
+
+class TestFederatedSparql(FederationMixin):
+    """Test the identifier mapping service."""
+
+    proc: Process
+    proc_blazegraph: Process
+
+    def assert_blazegraph_running(self):
+        """Check that blazegraph is running properly."""
+        self.assert_service_works(self.blazegraph_endpoint)
+
     def setUp(self):
         """Set up the test case."""
         self.blazegraph_jar_path = pystow.ensure(
@@ -110,14 +129,12 @@ class TestFederatedSparql(unittest.TestCase):
             url="https://github.com/blazegraph/database/releases/download/BLAZEGRAPH_2_1_6_RC/blazegraph.jar",
         )
         self.blazegraph_port = 9999
-        # maybe check setting port https://github.com/blazegraph/database/wiki/NanoSparqlServer#changing-the-default-port
-        # f"-Djetty.port={self.blazegraph_port}"
         self.blazegraph_endpoint = (
             f"http://localhost:{self.blazegraph_port}/blazegraph/namespace/kb/sparql"
         )
 
         try:
-            self.get(TEST_QUERY)
+            self.get(self.blazegraph_endpoint, TEST_QUERY)
         except requests.exceptions.ConnectionError:
             pass
         else:
@@ -177,7 +194,7 @@ class TestFederatedSparql(unittest.TestCase):
 
     def test_federated_local_blazegraph(self):
         """Test sending a federated query to a local mapping service from a local Blazegraph."""
-        resp = self.get(self.sparql)
+        resp = self.get(self.blazegraph_endpoint, self.sparql)
         try:
             res = resp.json()
             self.assertGreater(
@@ -195,7 +212,9 @@ class TestFederatedSparql(unittest.TestCase):
         # g = Graph(store="Oxigraph")
         # app = SparqlEndpoint(graph=g)
         # endpoint = TestClient(app)
-        # response = endpoint.get("/?query=" + urllib.parse.quote(FEDERATED_QUERY), headers={"accept": "application/json"})
+        # response = endpoint.get(
+        #     "/?query=" + urllib.parse.quote(FEDERATED_QUERY), headers={"accept": "application/json"}
+        # )
         # print(response.json()["results"]["bindings"])
         # assert response.status_code == 200
         # assert len(response.json()["results"]["bindings"]) > 0
