432fc066004469a8dc4fb84cf59063c9f474a71a
Vincent Emonet <vincent.emonet@gmail.com>
2023-04-14 08:43:26 +0200

    improve how triples are defined in init script

--- tests/resources/init_triplestores.sh
+++ tests/resources/init_triplestores.sh
@@ -1,9 +1,14 @@
 ## Script to initialize the triplestores started with docker
 # Run it from the root of the repo: ./tests/resources/init_triplestores.sh
 
+TRIPLES="
+<https://identifiers.org/CHEBI:24867> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/biolink/vocab/ChemicalEntity> .
+<https://identifiers.org/CHEBI:24868> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/biolink/vocab/ChemicalEntity> .
+"
+
 # Enable federated query for Virtuoso and load a triple for testing
 docker compose exec virtuoso isql -U dba -P dba exec='GRANT "SPARQL_SELECT_FED" TO "SPARQL";'
-docker compose exec virtuoso isql -U dba -P dba exec='SPARQL INSERT IN <https://identifiers.org/CHEBI> { <https://identifiers.org/CHEBI:24867> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/biolink/vocab/ChemicalEntity> };'
+docker compose exec virtuoso isql -U dba -P dba exec="SPARQL INSERT IN <https://identifiers.org/CHEBI> { $TRIPLES };"
 
 # Load a triple to local blazegraph for testing
-docker compose exec blazegraph curl -X POST http://localhost:8080/blazegraph/namespace/kb/sparql -d 'update=insert data {<https://identifiers.org/CHEBI:24867> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/biolink/vocab/ChemicalEntity> . }'
+docker compose exec blazegraph curl -X POST http://localhost:8080/blazegraph/namespace/kb/sparql -d "update=insert data { $TRIPLES }"
