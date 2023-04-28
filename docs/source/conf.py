974de71326fc69444e3de0ad37f97ad15ee0caf0
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-04-28 15:08:38 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -226,7 +226,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/3/": None,
+    "python": ("https://docs.python.org/3", None),
     "bioregistry": ("https://bioregistry.readthedocs.io/en/stable/", None),
     "pandas": ("https://pandas.pydata.org/docs/", None),
     "flask": ("https://flask.palletsprojects.com/", None),
