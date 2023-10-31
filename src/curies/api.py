956a1d46ba78cac44afe1e4f43cdf2ee2fb1bc13
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-10-31 14:53:13 +0100

    Improve JSON-LD and SHACL export (#85)
    This PR makes three improvements:
    
    1. JSON-LD export can be toggled to use [expanded term
    definitions](https://www.w3.org/TR/json-ld11/#expanded-term-definition)
    2. JSON-LD export can be toggled to use CURIE prefix synonyms
    3. SHACL export can be toggled to use CURIE prefix synonyms

--- src/curies/api.py
+++ src/curies/api.py
@@ -1832,25 +1832,58 @@ def _ensure_path(path: Union[str, Path]) -> Path:
     return path
 
 
-def write_jsonld_context(converter: Converter, path: Union[str, Path]) -> None:
+def write_jsonld_context(
+    converter: Converter,
+    path: Union[str, Path],
+    *,
+    include_synonyms: bool = False,
+    expand: bool = False,
+) -> None:
     """Write the converter's bijective map as a JSON-LD context to a file."""
     path = _ensure_path(path)
+    context = {}
+    for record in converter.records:
+        term = _get_expanded_term(record, expand=expand)
+        context[record.prefix] = term
+        if include_synonyms:
+            for prefix_synonym in record.prefix_synonyms:
+                context[prefix_synonym] = term
     with path.open("w") as file:
         json.dump(
             fp=file,
             indent=4,
             sort_keys=True,
-            obj={
-                "@context": converter.bimap,
-            },
+            obj={"@context": context},
         )
 
 
-def write_shacl(converter: Converter, path: Union[str, Path]) -> None:
+def _get_expanded_term(record: Record, *, expand: bool) -> Union[str, Dict[str, Any]]:
+    if not expand:
+        return record.uri_prefix
+
+    # Use expanded term definition described in https://www.w3.org/TR/json-ld11/#expanded-term-definition
+    rv = {"@prefix": True, "@id": record.uri_prefix}
+    # TODO add an @context inside here to somehow capture the pattern, if available
+    # if record.pattern:
+    #     rv["@context"] = {
+    #         "sh": "http://www.w3.org/ns/shacl#",
+    #         "sh:pattern": record.pattern,
+    #     }
+    return rv
+
+
+def write_shacl(
+    converter: Converter,
+    path: Union[str, Path],
+    *,
+    include_synonyms: bool = False,
+) -> None:
     """Write the converter's bijective map as SHACL in turtle RDF to a file.
 
     :param converter: The converter to export
     :param path: The path to a file to write to
+    :param include_synonyms: If true, includes CURIE prefix synonyms.
+        URI prefix synonyms are not output.
 
     .. seealso:: https://www.w3.org/TR/shacl/#sparql-prefixes
     """
@@ -1866,8 +1899,18 @@ def write_shacl(converter: Converter, path: Union[str, Path]) -> None:
         """
     )
     path = _ensure_path(path)
-    entries = ",\n".join(
-        f'    [ sh:prefix "{prefix}" ; sh:namespace "{uri_prefix}"^^xsd:anyURI ]'
-        for prefix, uri_prefix in sorted(converter.bimap.items())
-    )
-    path.write_text(text.format(entries=entries))
+    lines = []
+    for record in converter.records:
+        lines.append(_get_shacl_line(record.prefix, record.uri_prefix))
+        if include_synonyms:
+            for prefix_synonym in record.prefix_synonyms:
+                lines.append(_get_shacl_line(prefix_synonym, record.uri_prefix))
+    path.write_text(text.format(entries=",\n".join(lines)))
+
+
+def _get_shacl_line(prefix: str, uri_prefix: str) -> str:
+    line = f'    [ sh:prefix "{prefix}" ; sh:namespace "{uri_prefix}"^^xsd:anyURI '
+    # if pattern:
+    #     pattern = pattern.replace("\\", "\\\\")
+    #     line += f'; sh:pattern "{pattern}"'
+    return line + " ]"
