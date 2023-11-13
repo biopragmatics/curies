fb421491aa6b872509d92be4163ace19baa074f2
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-11-13 09:28:12 +0100

    Add initial discovery algorithm

---
+++ src/curies/discovery.py
@@ -0,0 +1,72 @@
+"""Discovery new entries for a Converter."""
+
+from typing import TYPE_CHECKING, Iterable
+
+from curies import Converter, Record
+from collections import defaultdict
+
+if TYPE_CHECKING:
+    import rdflib
+
+__all__ = [
+    "discover",
+    "discovery_rdflib",
+]
+
+
+def discovery_rdflib(converter: Converter, graph: "rdflib.Graph"):
+    return discover(converter, set(_yield_uris(graph)))
+
+
+def _yield_uris(graph: "rdflib.Graph") -> Iterable[str]:
+    import rdflib
+
+    for parts in graph.triples():
+        for part in parts:
+            if isinstance(part, rdflib.URIRef):
+                yield str(part)
+
+
+def discover(
+    converter: Converter,
+    uris: Iterable[str],
+    delimiters="#/",
+    cutoff: int = 30,
+    metaprefix: str = "ns",
+) -> Converter:
+    """Discover new URI prefixes.
+
+    :param converter: A converter with pre-existing definitions. URI prefixes
+        are considered "new" if they can't already be validated by this converter
+    :param uris: An iterable of URIs to search through. Will be taken as a set and
+        each unique entry is only considered once.
+    :param delimiters:
+        The delimiters considered between a putative URI prefix and putative
+        local unique identifier. By default, checks ``#`` first since this is
+        commonly used for URL fragments, then ``/`` since many URIs are constructed
+        with these.
+    :param cutoff: The number of unique URIs with a given prefix needed to call it
+        as unique. This can be adjusted, but is initially high.
+    :param metaprefix: The beginning part of each dummy prefix, followed by a number
+    :returns: A converter with dummy prefixes
+    """
+    counter = defaultdict(set)
+    for uri in uris:
+        if converter.is_uri(uri):
+            continue
+        for delimiter in delimiters:
+            if delimiter not in uri:
+                continue
+            uri_prefix, luid = uri.rsplit(delimiter, maxsplit=1)
+            if luid.isalnum():
+                counter[uri_prefix + delimiter].add(luid)
+                break
+    counter = dict(counter)
+    records = []
+    record_number = 0
+    for uri_prefix, luids in sorted(counter.items()):
+        if len(luids) > cutoff:
+            record_number += 1
+            prefix = f"{metaprefix}{record_number}"
+            records.append(Record(prefix=prefix, uri_prefix=uri_prefix))
+    return Converter(records)
---
+++ tests/test_discovery.py
@@ -0,0 +1,27 @@
+"""Test discovering a prefix map from a list of URIs."""
+
+import unittest
+from typing import ClassVar
+
+from curies.discovery import discover
+from curies import Converter, Record
+
+
+class TestDiscovery(unittest.TestCase):
+    """Test discovery of URI prefixes."""
+
+    converter: ClassVar[Converter]
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up the test case with a dummy converter."""
+        cls.converter = Converter([])
+
+    def test_simple(self):
+        """Test a simple case of discovering URI prefixes."""
+        uris = [f"http://ran.dom/{i:03}" for i in range(30)]
+        converter = discover(self.converter, uris, cutoff=3)
+        self.assertEqual([Record(prefix="ns1", uri_prefix="http://ran.dom/")], converter.records)
+
+        converter = discover(self.converter, uris, cutoff=50)
+        self.assertEqual([], converter.records)
