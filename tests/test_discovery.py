c0e61790127c795c423536f97d65aa0456cf6988
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-11-13 09:43:58 +0100

    Update test_discovery.py

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -15,13 +15,26 @@ class TestDiscovery(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         """Set up the test case with a dummy converter."""
-        cls.converter = Converter([])
+        cls.converter = Converter(
+            [Record(prefix="GO", uri_prefix="http://purl.obolibrary.org/obo/GO_")]
+        )
 
     def test_simple(self):
         """Test a simple case of discovering URI prefixes."""
         uris = [f"http://ran.dom/{i:03}" for i in range(30)]
+        uris.append("http://purl.obolibrary.org/obo/GO_0001234")
+
         converter = discover(self.converter, uris, cutoff=3)
         self.assertEqual([Record(prefix="ns1", uri_prefix="http://ran.dom/")], converter.records)
+        self.assertEqual("ns1:001", converter.compress("http://ran.dom/001"))
+        self.assertIsNone(
+            converter.compress("http://purl.obolibrary.org/obo/GO_0001234"),
+            msg="discovered converter should not inherit reference converter's definitions",
+        )
 
         converter = discover(self.converter, uris, cutoff=50)
         self.assertEqual([], converter.records)
+        self.assertIsNone(
+            converter.compress("http://ran.dom/001"),
+            msg="cutoff was high, so discovered converter should not detect `http://ran.dom/`",
+        )
