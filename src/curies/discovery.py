7d8c07331bb0b2759ae57a42e437e0779753c657
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-11-13 15:20:57 +0100

    Update docs and refactor

--- src/curies/discovery.py
+++ src/curies/discovery.py
@@ -1,7 +1,10 @@
 """Discovery new entries for a Converter."""
 
 from collections import defaultdict
-from typing import TYPE_CHECKING, Any, Iterable, Mapping, Optional, Sequence, Set, Union
+from pathlib import PurePath
+from typing import IO, TYPE_CHECKING, Any, Iterable, Mapping, Optional, Sequence, Set, TextIO, Union
+
+from typing_extensions import Literal
 
 from curies import Converter, Record
 
@@ -14,27 +17,53 @@ __all__ = [
 ]
 
 
+GraphFormats = Literal["turtle", "xml", "n3", "nt", "trix"]
+GraphInput = Union[IO[bytes], TextIO, "rdflib.parser.InputSource", str, bytes, PurePath]
+
+
 def discover_from_rdf(
     converter: Converter,
-    graph: Union[str, "rdflib.Graph"],
-    graph_format: Optional[str] = None,
+    graph: Union[GraphInput, "rdflib.Graph"],
+    *,
+    format: Optional[GraphFormats] = None,
     **kwargs: Any,
 ) -> Converter:
-    """Discover new URI prefixes from an RDFLib triple store, wrapping :func:`curies.discover`."""
-    graph = _ensure_graph(graph, graph_format)
+    """Discover new URI prefixes from RDF content via :mod:`rdflib`.
+
+    :param converter:
+        A converter with pre-existing definitions. URI prefixes
+        are considered "new" if they can't already be validated by this converter
+    :param graph:
+        Either a pre-instantiated RDFlib graph, or an input type to the ``source``
+        keyword of :meth:`rdflib.Graph.parse`. This can be one of the following:
+
+        - A string or bytes representation of a URL
+        - A string, bytes, or Path representation of a local file
+        - An I/O object that can be read directly
+        - An open XML reader from RDFlib (:class:`rdflib.parser.InputSource`)
+    :param format: If ``graph`` is given as a URL or I/O object, this
+        is passed through to the ``format`` keyword of :meth:`rdflib.Graph.parse`.
+        If none is given, defaults to ``turtle``.
+    :param kwargs: Keyword arguments passed through to :func:`discover`
+    :returns:
+        A converter with dummy prefixes for URI prefixes appearing in the RDF
+        content (i.e., triples).
+    """
+    graph = _ensure_graph(graph, format)
     uris = set(_yield_uris(graph))
     return discover(converter, uris, **kwargs)
 
 
 def _ensure_graph(
-    graph: Union[str, "rdflib.Graph"], graph_format: Optional[str] = None
+    graph: Union[GraphInput, "rdflib.Graph"], format: Optional[GraphFormats] = None
 ) -> "rdflib.Graph":
-    if not isinstance(graph, str):
-        return graph
     import rdflib
 
+    if isinstance(graph, rdflib.Graph):
+        return graph
+
     rv = rdflib.Graph()
-    rv.parse(graph, format=graph_format)
+    rv.parse(graph, format=format)
     return rv
 
 
@@ -50,8 +79,9 @@ def _yield_uris(graph: "rdflib.Graph") -> Iterable[str]:
 def discover(
     converter: Converter,
     uris: Iterable[str],
+    *,
     delimiters: Sequence[str] = "#/",
-    cutoff: int = 30,
+    cutoff: Optional[int] = None,
     metaprefix: str = "ns",
 ) -> Converter:
     """Discover new URI prefixes.
@@ -70,27 +100,38 @@ def discover(
     :param cutoff:
         The number of unique URIs with a given prefix needed to call it
         as unique. This can be adjusted, but is initially high.
+
+        The cutoff can be set to 0 to recover all putative URI prefixes.
+        However, this comes with the risk of false positives.
     :param metaprefix:
         The beginning part of each dummy prefix, followed by a number. The default value
         is ``ns``, so dummy prefixes are named ``ns1``, ``ns2``, and so on.
     :returns:
         A converter with dummy prefixes
     """
-    counter = discover_helper(converter=converter, uris=uris, delimiters=delimiters)
-    records = []
-    record_number = 0
-    for uri_prefix, luids in sorted(counter.items()):
-        if len(luids) > cutoff:
-            record_number += 1
-            prefix = f"{metaprefix}{record_number}"
-            records.append(Record(prefix=prefix, uri_prefix=uri_prefix))
+    if cutoff is None:
+        cutoff = 0
+    uri_prefix_to_luids = _get_uri_prefix_to_luids(
+        converter=converter, uris=uris, delimiters=delimiters
+    )
+    uri_prefixes = [
+        uri_prefix
+        for uri_prefix, luids in sorted(uri_prefix_to_luids.items())
+        # If the cutoff is 5, and only 3 unique LUIDs with the URI prefix
+        # were identified, we're going to disregard this URI prefix.
+        if len(luids) >= cutoff
+    ]
+    records = [
+        Record(prefix=f"{metaprefix}{uri_prefix_index}", uri_prefix=uri_prefix)
+        for uri_prefix_index, uri_prefix in enumerate(uri_prefixes, start=1)
+    ]
     return Converter(records)
 
 
-def discover_helper(
+def _get_uri_prefix_to_luids(
     converter: Converter, uris: Iterable[str], delimiters: Sequence[str] = "#/"
 ) -> Mapping[str, Set[str]]:
-    """Discover new URI prefixes.
+    """Get a mapping from putative URI prefixes to corresponding putative local unique identifiers.
 
     :param converter:
         A converter with pre-existing definitions. URI prefixes
@@ -106,7 +147,7 @@ def discover_helper(
     :returns:
         A dictionary of putative URI prefixes to sets of putative local unique identifiers
     """
-    counter = defaultdict(set)
+    uri_prefix_to_luids = defaultdict(set)
     for uri in uris:
         if converter.is_uri(uri):
             continue
@@ -115,6 +156,6 @@ def discover_helper(
                 continue
             uri_prefix, luid = uri.rsplit(delimiter, maxsplit=1)
             if luid.isalnum():
-                counter[uri_prefix + delimiter].add(luid)
+                uri_prefix_to_luids[uri_prefix + delimiter].add(luid)
                 break
-    return dict(counter)
+    return dict(uri_prefix_to_luids)
