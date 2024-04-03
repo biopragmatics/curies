136549e775abd678f38191a2c4a6f27e09b68860
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-04-03 09:58:29 +0200

    Update api.py

--- src/curies/api.py
+++ src/curies/api.py
@@ -36,7 +36,8 @@ import requests
 from pydantic import BaseModel, Field
 from pytrie import StringTrie
 
-from ._pydantic_compat import field_validator, get_field_validator_values, PYDANTIC_V1
+from ._pydantic_compat import PYDANTIC_V1, field_validator, get_field_validator_values
+
 if not PYDANTIC_V1:
     from pydantic import ConfigDict
 
@@ -204,12 +205,14 @@ class Reference(BaseModel):  # type:ignore
     )
 
     if PYDANTIC_V1:
+
         class Config:
             """Pydantic configuration for references."""
+
             frozen = True
+
     else:
         model_config = ConfigDict(frozen=True)
-
 
     @property
     def curie(self) -> str:
