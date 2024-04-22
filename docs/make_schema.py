5fe2a21790f3470e65f10515859975f6a1911371
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-04-22 13:50:41 +0200

    Update records

--- docs/make_schema.py
+++ docs/make_schema.py
@@ -1,52 +1,46 @@
-import pydantic
-import pydantic.schema
-from typing import List
+"""Generate a JSON schema for extended prefix maps."""
 
-from pydantic import BaseModel, ConfigDict
-
-from curies import Record
-from pathlib import Path
 import json
+from pathlib import Path
+
+from curies import Records
+from curies._pydantic_compat import PYDANTIC_V1
 
 HERE = Path(__file__).parent.resolve()
-PATH = HERE.joinpath("path.json")
+PATH = HERE.joinpath("schema.json")
 TITLE = "Extended Prefix Map"
 DESCRIPTION = ""
+URL = "https://w3id.org/biopragmatics/schema/epm.json"
 
 
-class Records(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    __root__ = List[Record]
-
-
-def main():
+def main() -> None:
+    """Generate a JSON schema for extended prefix maps."""
     rv = {
         "$schema": "http://json-schema.org/draft-07/schema#",
-        "$id": "https://bioregistry.io/schema.json",
+        "$id": URL,
     }
 
-    try:
+    if PYDANTIC_V1:
+        import pydantic.schema
+
         # see https://docs.pydantic.dev/latest/usage/json_schema/#general-notes-on-json-schema-generation
-        from pydantic.json_schema import models_json_schema
-    except ImportError:
+
         schema_dict = pydantic.schema.schema(
             [Records],
             title=TITLE,
             description=DESCRIPTION,
         )
     else:
+        from pydantic.json_schema import models_json_schema
+
         _, schema_dict = models_json_schema(
             [(Records, "validation")],
             title=TITLE,
             description=DESCRIPTION,
         )
-    rv.update(schema_dict)
 
-    PATH.write_text(json.dumps(rv, indent=2))
+    rv.update(schema_dict)
+    PATH.write_text(json.dumps(rv, indent=2) + "\n")
 
 
 if __name__ == "__main__":
