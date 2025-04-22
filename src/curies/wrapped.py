7ad4258bc6d86e95130cf13f553c180c2be0b9f9
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-22 16:19:02 +0200

    Update wrapped.py

--- src/curies/wrapped.py
+++ src/curies/wrapped.py
@@ -1,19 +1,20 @@
 """Reusable configuration."""
 
-from typing import TypeVar
+import json
 from pathlib import Path
+from typing import Any, Literal, TypeVar, overload
 
-from curies import ReferenceTuple
 from pydantic import BaseModel, Field
-from .api import Reference, Converter
 from typing_extensions import Self
+
+from .api import Converter, Reference, ReferenceTuple
 
 __all__ = [
     "Blacklist",
-    "Rewrites",
-    "Rules",
     "BlacklistError",
     "PreprocessingConverter",
+    "Rewrites",
+    "Rules",
 ]
 
 X = TypeVar("X", bound=Reference)
@@ -61,6 +62,16 @@ class Blacklist(BaseModel):
             return True
         return str_or_curie_or_uri in self.full
 
+    def str_is_blacklisted(
+        self, str_or_curie_or_uri: str, *, ontology_prefix: str | None = None
+    ) -> bool:
+        """Check if the full CURIE string is blacklisted."""
+        return (
+            self.str_has_blacklisted_prefix(str_or_curie_or_uri, ontology_prefix=ontology_prefix)
+            or self.str_has_blacklisted_suffix(str_or_curie_or_uri)
+            or self.str_is_blacklisted_full(str_or_curie_or_uri, ontology_prefix=ontology_prefix)
+        )
+
 
 class Rewrites(BaseModel):
     """A model for prefix and full rewrites."""
@@ -93,10 +104,10 @@ class Rewrites(BaseModel):
         if ontology_prefix is not None:
             for old_prefix, new_prefix in self.resource_prefix.get(ontology_prefix, {}).items():
                 if str_or_curie_or_uri.startswith(old_prefix):
-                    return new_prefix + str_or_curie_or_uri[len(old_prefix):]
+                    return new_prefix + str_or_curie_or_uri[len(old_prefix) :]
         for old_prefix, new_prefix in self.prefix.items():
             if str_or_curie_or_uri.startswith(old_prefix):
-                return new_prefix + str_or_curie_or_uri[len(old_prefix):]
+                return new_prefix + str_or_curie_or_uri[len(old_prefix) :]
         return str_or_curie_or_uri
 
 
@@ -114,23 +125,11 @@ class Rules(BaseModel):
         rules.blacklists._sort()
         path.write_text(json.dumps(rules.model_dump(), sort_keys=True, indent=2))
 
-    def str_has_blacklisted_prefix(
+    def str_is_blacklisted(
         self, str_or_curie_or_uri: str, *, ontology_prefix: str | None = None
     ) -> bool:
-        """Check if the CURIE string has a blacklisted prefix."""
-        return self.blacklists.str_has_blacklisted_prefix(
-            str_or_curie_or_uri, ontology_prefix=ontology_prefix
-        )
-
-    def str_has_blacklisted_suffix(self, str_or_curie_or_uri: str) -> bool:
-        """Check if the CURIE string has a blacklisted suffix."""
-        return self.blacklists.str_has_blacklisted_suffix(str_or_curie_or_uri)
-
-    def str_is_blacklisted_full(
-        self, str_or_curie_or_uri: str, *, ontology_prefix: str | None = None
-    ) -> bool:
-        """Check if the full CURIE string is blacklisted."""
-        return self.blacklists.str_is_blacklisted_full(
+        """Check if the CURIE string is blacklisted."""
+        return self.blacklists.str_is_blacklisted(
             str_or_curie_or_uri, ontology_prefix=ontology_prefix
         )
 
@@ -150,7 +149,7 @@ class Rules(BaseModel):
 def _load_rules(rules: str | Path | Rules) -> Rules:
     if isinstance(rules, str | Path):
         rules = Path(rules).expanduser().resolve()
-        rules = Rules.model_validate_json(rules)
+        rules = Rules.model_validate_json(rules.read_text())
     return rules
 
 
@@ -161,17 +160,45 @@ class BlacklistError(ValueError):
 class PreprocessingConverter(Converter):
     """A converter with pre-processing rules."""
 
-    def __init__(self, *args: Any, rules: Rules | str | Path, **kwargs) -> None:
+    def __init__(self, *args: Any, rules: Rules | str | Path, **kwargs: Any) -> None:
+        """Instantiate a converter with a ruleset for pre-processing.
+
+        :param args: Positional arguments passed to :func:`Converter.__init__`
+        :param rules: A set of rules
+        :param kwargs: Keyword arguments passed to :func:`Converter.__init__`
+        """
         super().__init__(*args, **kwargs)
         self.rules = _load_rules(rules)
+        self._cls = Reference
 
+    @classmethod
     def from_converter(cls, converter: Converter, rules: Rules | str | Path) -> Self:
+        """Wrap a converter with a ruleset."""
         rules = _load_rules(rules)
-        return cls(rcords=converter.records, rules=rules)
+        return cls(records=converter.records, rules=rules)
 
-    def parse(self, uri_or_curie: str, *, strict: bool, ontology_prefix: str | None = None) -> ReferenceTuple | None:
-        if r1 := self.rules.remap_full(uri_or_curie, ontology_prefix=ontology_prefix):
-            return r1
+    @overload
+    def parse(
+        self, uri_or_curie: str, *, strict: Literal[True] = True, ontology_prefix: str | None = ...
+    ) -> ReferenceTuple: ...
+
+    @overload
+    def parse(
+        self,
+        uri_or_curie: str,
+        *,
+        strict: Literal[False] = False,
+        ontology_prefix: str | None = ...,
+    ) -> ReferenceTuple | None: ...
+
+    def parse(
+        self, uri_or_curie: str, *, strict: bool = False, ontology_prefix: str | None = None
+    ) -> ReferenceTuple | None:
+        """Parse a string, CURIE, or URI."""
+        if r1 := self.rules.remap_full(
+            uri_or_curie, cls=self._cls, ontology_prefix=ontology_prefix
+        ):
+            return r1.pair
 
         # Remap node's prefix (if necessary)
         uri_or_curie = self.rules.remap_prefix(uri_or_curie, ontology_prefix=ontology_prefix)
@@ -179,4 +206,6 @@ class PreprocessingConverter(Converter):
         if self.rules.str_is_blacklisted(uri_or_curie, ontology_prefix=ontology_prefix):
             raise BlacklistError
 
+        if strict:
+            return super().parse(uri_or_curie, strict=strict)
         return super().parse(uri_or_curie, strict=strict)
