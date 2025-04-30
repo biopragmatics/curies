fef02b70f9e38b5b36fe785a56bf19f2f0b922c7
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-30 14:14:02 +0200

    Update triples
    - Add named tuple for string triple
    - Add sortability for triples

--- src/curies/triples.py
+++ src/curies/triples.py
@@ -7,7 +7,7 @@ import gzip
 from collections.abc import Generator, Iterable, Sequence
 from contextlib import contextmanager
 from pathlib import Path
-from typing import TextIO
+from typing import TextIO, NamedTuple
 
 from pydantic import BaseModel, ConfigDict
 from typing_extensions import Self
@@ -17,8 +17,17 @@ from .api import Reference
 __all__ = [
     "Triple",
     "read_triples",
+    "StrTriple",
     "write_triples",
 ]
+
+
+class StrTriple(NamedTuple):
+    """A triple of curies."""
+
+    subject: str
+    predicate: str
+    object: str
 
 
 class Triple(BaseModel):
@@ -30,9 +39,12 @@ class Triple(BaseModel):
     predicate: Reference
     object: Reference
 
-    def as_curies(self) -> tuple[str, str, str]:
+    def as_curies(self) -> StrTriple:
         """Get a three-tuple of strings representing this triple."""
-        return self.subject.curie, self.predicate.curie, self.object.curie
+        return StrTriple(self.subject.curie, self.predicate.curie, self.object.curie)
+
+    def __lt__(self, other: Triple) -> bool:
+        return self.as_curies() < other.as_curies()
 
     @classmethod
     def from_curies(
