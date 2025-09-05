4f55f3067b2b72f28162ad122b88051f4cae99e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-09-05 13:13:57 +0200

    Improve implementations

--- src/curies/dataframe.py
+++ src/curies/dataframe.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import itertools as itt
+from collections import defaultdict
 from collections.abc import Collection, Iterable
 from typing import TYPE_CHECKING, Callable, Literal, TypeAlias
 
@@ -13,7 +14,11 @@ if TYPE_CHECKING:
     import sssom
 
 __all__ = [
+    "filter_df_curie",
     "filter_df_prefix",
+    "get_curie_index",
+    "get_dense_curie",
+    "get_dense_prefix",
     "get_prefix_index",
 ]
 
@@ -40,14 +45,29 @@ def _get_prefix_checker(prefix: str | Collection[str]) -> Callable[[str], bool]:
 def _get_prefixes_from_curie_column(
     df: pd.DataFrame, column: int | str, converter: Converter, validate: bool = True
 ) -> pd.Series:
+    return df[column].map(_get_parse_curie(converter=converter, validate=validate))
+
+
+def _get_parse_curie(
+    *, converter: Converter | None = None, validate: bool = False
+) -> Callable[[str], str]:
     # TODO what if it can't parse?
     # TODO handle None?
     # TODO handle invalid CURIEs?
 
-    if validate:
-        return df[column].map(lambda curie: converter.parse_curie(curie, strict=True).prefix)
+    if not validate:
+
+        def _func(curie: str) -> str:
+            return _split(curie)[0]
+    elif converter is None:
+        raise ValueError("converter is required for validation")
     else:
-        return df[column].map(lambda curie: _split(curie)[0])
+
+        def _func(curie: str) -> str:
+            reference = converter.parse_curie(curie, strict=True)
+            return reference.prefix
+
+    return _func
 
 
 Method: TypeAlias = Literal["a", "b"]
@@ -62,24 +82,24 @@ def get_prefix_index(
     converter: Converter | None = None,
 ) -> pd.Series:
     """Get an index of CURIEs in the given column that start with the prefix(es)."""
-    if method == "a":
+    if method == "a" or method is None:
         return df[column].map(_get_prefix_checker(prefix))
     elif method == "b":
-        if converter is None:
+        if converter is None:  # pragma: no cover
             raise ValueError("a converter is required for method B")
         prefix_series = _get_prefixes_from_curie_column(df, column, converter)
         if isinstance(prefix, str):
             return prefix_series == prefix
         else:
             return prefix_series.isin(prefix)
-    else:
-        raise ValueError
+    else:  # pragma: no cover
+        raise ValueError(f"invalid method given: {method}")
 
 
 def filter_df_prefix(
     df: pd.DataFrame,
     column: str | int,
-    prefix: str,
+    prefix: str | Collection[str],
     *,
     method: Method | None = None,
     converter: Converter | None = None,
@@ -95,6 +115,59 @@ def filter_df_prefix(
     """
     idx = get_prefix_index(df=df, column=column, prefix=prefix, method=method, converter=converter)
     return df[idx]
+
+
+def get_curie_index(
+    df: pd.DataFrame,
+    column: str | int,
+    curie: str | Collection[str],
+) -> pd.Series:
+    """Get an index of CURIEs in the given column that are the given CURIE(s)."""
+    if isinstance(curie, str):
+        return df[column] == curie
+    else:
+        return df[column].isin(set(curie))
+
+
+def filter_df_curie(
+    df: pd.DataFrame,
+    column: str | int,
+    curie: str | Collection[str],
+) -> pd.DataFrame:
+    """Filter a dataframe based on CURIEs in a given column having a given prefix or set of prefixes.
+
+    :param df: A dataframe
+    :param column:
+        The integer index or column name of a column containing CURIEs
+    :param curie:
+        The CURIE (given as a string) or collection of CURIEs (given as a list, set, etc.) to keep
+    :returns: If not in place, return a new dataframe.
+    """
+    idx = get_curie_index(df=df, column=column, curie=curie)
+    return df[idx]
+
+
+def get_dense_prefix(
+    df: pd.DataFrame,
+    column: str | int,
+    *,
+    converter: Converter | None = None,
+    validate: bool = False,
+) -> dict[str, list[int]]:
+    """Get a dictionary from prefixes that appear in the column to the row indexes where they appear."""
+    dd: defaultdict[str, list[int]] = defaultdict(list)
+    f = _get_parse_curie(converter=converter, validate=validate)
+    for i, prefix in enumerate(df[column].map(f)):
+        dd[prefix].append(i)
+    return dict(dd)
+
+
+def get_dense_curie(df: pd.DataFrame, column: str | int) -> dict[str, list[int]]:
+    """Get a dictionary from CURIEs that appear in the column to the row indexes where they appear."""
+    dd: defaultdict[str, list[int]] = defaultdict(list)
+    for i, curie in enumerate(df[column]):
+        dd[curie].append(i)
+    return dict(dd)
 
 
 def split_msdf_by_prefix(
@@ -124,20 +197,45 @@ def _split_dataframe_by_prefix(
     subject_prefixes: Collection[str],
     predicates: Collection[str],
     object_prefixes: Collection[str],
+    *,
+    method: Literal[1, 2] = 1,
 ) -> Iterable[tuple[tuple[str, str, str], pd.DataFrame]]:
-    s_indexes = {
-        subject_prefix: get_prefix_index(df, column="subject_id", prefix=subject_prefix)
-        for subject_prefix in subject_prefixes
-    }
-    p_indexes = {predicate: df["predicate_id"] == predicate for predicate in predicates}
-    o_indexes = {
-        object_prefix: get_prefix_index(df, column="object_id", prefix=object_prefix)
-        for object_prefix in object_prefixes
-    }
-    for subject_prefix, predicate, object_prefix in itt.product(
-        subject_prefixes, predicates, object_prefixes
-    ):
-        idx = s_indexes[subject_prefix] & p_indexes[predicate] & o_indexes[object_prefix]
-        if not idx.any():
-            continue
-        yield (subject_prefix, predicate, object_prefix), df[idx]
+    if method == 1:
+        s_indexes = {
+            subject_prefix: get_prefix_index(df, column="subject_id", prefix=subject_prefix)
+            for subject_prefix in subject_prefixes
+        }
+        p_indexes = {
+            predicate: get_curie_index(df, column="predicate_id", curie=predicate)
+            for predicate in predicates
+        }
+        o_indexes = {
+            object_prefix: get_prefix_index(df, column="object_id", prefix=object_prefix)
+            for object_prefix in object_prefixes
+        }
+        for subject_prefix, predicate, object_prefix in itt.product(
+            subject_prefixes, predicates, object_prefixes
+        ):
+            idx = s_indexes[subject_prefix] & p_indexes[predicate] & o_indexes[object_prefix]
+            if not idx.any():
+                continue
+            yield (subject_prefix, predicate, object_prefix), df[idx]
+
+    elif method == 2:
+        s_index = get_dense_prefix(df, "subject_id")
+        p_index = get_dense_curie(df, "predicate_id")
+        o_index = get_dense_prefix(df, "object_id")
+        for subject_prefix, predicate, object_prefix in itt.product(
+            subject_prefixes, predicates, object_prefixes
+        ):
+            method_2_idx: list[int] = sorted(
+                set(s_index.get(subject_prefix, []))
+                .intersection(p_index.get(predicate, []))
+                .intersection(o_index.get(object_prefix, []))
+            )
+            if not method_2_idx:
+                continue
+            yield (subject_prefix, predicate, object_prefix), df.iloc[method_2_idx]
+
+    else:
+        raise ValueError
--- tests/test_dataframe.py
+++ tests/test_dataframe.py
@@ -6,7 +6,14 @@ import unittest
 import pandas as pd
 
 from curies import Converter
-from curies.dataframe import Method, get_prefix_index
+from curies.dataframe import (
+    Method,
+    filter_df_curie,
+    filter_df_prefix,
+    get_dense_curie,
+    get_dense_prefix,
+    get_prefix_index,
+)
 
 CONVERTER = Converter.from_prefix_map(
     {
@@ -22,21 +29,51 @@ class TestDataframe(unittest.TestCase):
 
     def test_get_prefix_index(self) -> None:
         """Test getting a prefix index."""
-        curies = [
-            *(f"a:{i}" for i in range(5)),
-            *(f"b:{i}" for i in range(5)),
-            *(f"c:{i}" for i in range(5)),
-        ]
+        column = "curie"
+        a_curies = [f"a:{i}" for i in range(5)]
+        b_curies = [f"b:{i}" for i in range(5)]
+        c_curies = [f"c:{i}" for i in range(5)] * 2
+        curies = [*a_curies, *b_curies, *c_curies]
         rows = [(curie,) for curie in curies]
-        df = pd.DataFrame(rows, columns=["curie"])
+        df = pd.DataFrame(rows, columns=[column])
 
         for method in typing.get_args(Method):
             with self.subTest(method=method):
-                idx = get_prefix_index(df, "curie", "a", method=method, converter=CONVERTER)
+                idx = get_prefix_index(df, column, "a", method=method, converter=CONVERTER)
                 self.assertEqual([0, 1, 2, 3, 4], _rr(idx))
 
-                idx = get_prefix_index(df, "curie", ["a", "b"], method=method, converter=CONVERTER)
+                idx = get_prefix_index(df, column, ["a", "b"], method=method, converter=CONVERTER)
                 self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], _rr(idx))
+
+                df_a = filter_df_prefix(df, column, "a")
+                self.assertEqual(set(a_curies), set(df_a[column]))
+
+                df_ab = filter_df_prefix(df, column, ["a", "b"])
+                self.assertEqual({*a_curies, *b_curies}, set(df_ab[column]))
+
+        df_a1 = filter_df_curie(df, column, "a:1")
+        self.assertEqual({"a:1"}, set(df_a1[column]))
+
+        df_a123 = filter_df_curie(df, column, ["a:1", "a:2", "b:1"])
+        self.assertEqual({"a:1", "a:2", "b:1"}, set(df_a123[column]))
+
+        dense_prefix_mapping = get_dense_prefix(df, column)
+        self.assertEqual(
+            {
+                "a": [0, 1, 2, 3, 4],
+                "b": [5, 6, 7, 8, 9],
+                "c": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+            },
+            dense_prefix_mapping,
+        )
+
+        dense_curie_mapping = get_dense_curie(df, column)
+        self.assertNotIn("a", dense_curie_mapping)
+        self.assertNotIn("b", dense_curie_mapping)
+        self.assertNotIn("c", dense_curie_mapping)
+        self.assertIn("a:0", dense_curie_mapping)
+        self.assertEqual([0], dense_curie_mapping["a:0"])
+        self.assertEqual([10, 15], dense_curie_mapping["c:0"])
 
 
 def _rr(series: pd.Series) -> list[int]:
