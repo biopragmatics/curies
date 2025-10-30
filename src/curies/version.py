3bf3271dd2424109f2b955959216a0346bbfb7c8
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-30 10:31:31 +0100

    Add version getting

--- src/curies/version.py
+++ src/curies/version.py
@@ -1,5 +1,8 @@
 """Version information for :mod:`curies`."""
 
+import os
+from subprocess import CalledProcessError, check_output
+
 __all__ = [
     "VERSION",
     "get_version",
@@ -8,6 +11,25 @@ __all__ = [
 VERSION = "0.12.4-dev"
 
 
-def get_version() -> str:
-    """Get the :mod:`curies` version string."""
-    return VERSION
+def get_git_hash() -> str:
+    """Get the :mod:`curies` git hash."""
+    with open(os.devnull, "w") as devnull:
+        try:
+            ret = check_output(
+                ["git", "rev-parse", "HEAD"],
+                cwd=os.path.dirname(__file__),
+                stderr=devnull,
+            )
+        except CalledProcessError:
+            return "UNHASHED"
+        else:
+            return ret.strip().decode("utf-8")[:8]
+
+
+def get_version(with_git_hash: bool = False) -> str:
+    """Get the :mod:`curies` version string, including a git hash."""
+    return f"{VERSION}-{get_git_hash()}" if with_git_hash else VERSION
+
+
+if __name__ == "__main__":
+    print(get_version(with_git_hash=True))  # noqa:T201
