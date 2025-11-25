2e1839419af0e928c08ffde64da1c2034e4e544f
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-25 13:28:13 +0100

    Update mixins.py

--- src/curies/mixins.py
+++ src/curies/mixins.py
@@ -11,6 +11,7 @@ from .api import Converter
 
 __all__ = [
     "SemanticallyProcessable",
+    "SemanticallyStandardizable",
 ]
 
 X = TypeVar("X")
@@ -50,7 +51,67 @@ class SemanticallyProcessable(ABC, Generic[X]):
 
 
 class SemanticallyStandardizable(ABC):
-    """An object that can be standardized."""
+    """An object that can be standardized.
+
+    In the following example, a simple object is constructed:
+
+    .. code-block:: python
+
+        from typing_extensions import Self
+        from curies import Converter, Reference, SemanticallyStandardizable
+
+
+        class ReferenceHolder(SemanticallyStandardizable):
+            def __init__(self, reference):
+                self.reference = reference
+
+            def standardize(self, converter: Converter) -> Self:
+                return ReferenceHolder(converter.standardize_reference(self.reference, strict=True))
+
+    It's good form to make these operations return new objects, but there's no reason
+    you couldn't update the object in place like in :
+
+    .. code-block:: python
+
+        from typing_extensions import Self
+        from curies import Converter, Reference, SemanticallyStandardizable
+
+
+        class ReferenceHolder(SemanticallyStandardizable):
+            def __init__(self, reference):
+                self.reference = reference
+
+            def standardize(self, converter: Converter) -> Self:
+                self.reference = converter.standardize_reference(self.reference, strict=True)
+                return self
+
+    In the following example, the :meth:`pydantic.BaseModel.model_copy` is
+    used to automatically reuse all other fields that aren't updated, which
+    creates a new object.
+
+    .. code-block:: python
+
+        import datetime
+        from typing_extensions import Self
+        from curies import Converter, Reference, SemanticallyStandardizable
+        from pydantic import BaseModel
+
+
+        class Triple(BaseModel, SemanticallyStandardizable):
+            subject: Reference
+            predicate: Reference
+            object: Reference
+            date_asserted: datetime.date
+
+            def standardize(self, converter: Converter) -> Self:
+                return self.model_copy(
+                    update={
+                        "subject": converter.standardize_reference(self.subject, strict=True),
+                        "predicate": converter.standardize_reference(self.predicate, strict=True),
+                        "object": converter.standardize_reference(self.object, strict=True),
+                    }
+                )
+    """
 
     @abstractmethod
     def standardize(self, converter: Converter) -> Self:
