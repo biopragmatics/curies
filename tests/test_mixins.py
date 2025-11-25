5b680278ebfd1e78b5fcdcc544d834e8ee451d31
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-11-25 13:38:32 +0100

    Add mixin for standardizing (#204)

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -3,9 +3,11 @@
 import unittest
 
 from pydantic import BaseModel
+from typing_extensions import Self
 
+import curies
 from curies import Converter, Reference
-from curies.mixins import SemanticallyProcessable
+from curies.mixins import SemanticallyProcessable, SemanticallyStandardizable
 
 
 class TestMixins(unittest.TestCase):
@@ -33,3 +35,30 @@ class TestMixins(unittest.TestCase):
         p = v.process(c)
         self.assertIsInstance(p, Processed)
         self.assertEqual(Reference(prefix="GO", identifier="1234567"), p.reference)
+
+    def test_standardizable(self) -> None:
+        """Test standardizable."""
+
+        class HoldsReference(BaseModel, SemanticallyStandardizable):
+            """A test class with a reference."""
+
+            reference: Reference
+
+            def standardize(self, converter: Converter) -> Self:
+                """Standardize the reference in the object."""
+                return self.model_copy(
+                    update={
+                        "reference": converter.standardize_reference(self.reference, strict=True),
+                    }
+                )
+
+        converter = Converter(
+            [
+                curies.Record(
+                    prefix="TEST", prefix_synonyms=["test"], uri_prefix="https://example.org/"
+                )
+            ]
+        )
+        init = HoldsReference(reference=Reference(prefix="test", identifier="1234567"))
+        end = init.standardize(converter)
+        self.assertEqual(Reference(prefix="TEST", identifier="1234567"), end.reference)
