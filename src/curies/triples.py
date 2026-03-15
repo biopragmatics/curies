dfcddd1abb915043b190fb05bd29c5a7cbe7de9c
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-03-15 10:46:53 +0100

    Consolidate

--- src/curies/triples.py
+++ src/curies/triples.py
@@ -2,21 +2,24 @@
 
 from __future__ import annotations
 
+import base64
 import csv
 import gzip
 from collections.abc import Generator, Iterable, Sequence
 from contextlib import contextmanager
 from pathlib import Path
-from typing import NamedTuple, TextIO
+from typing import NamedTuple, TextIO, cast
 
 from pydantic import BaseModel, ConfigDict
 from typing_extensions import Self
 
-from .api import Reference
+from .api import Converter, Reference
 
 __all__ = [
     "StrTriple",
     "Triple",
+    "decode_triple",
+    "encode_triple",
     "read_triples",
     "write_triples",
 ]
@@ -42,6 +45,14 @@ class Triple(BaseModel):
     def as_str_triple(self) -> StrTriple:
         """Get a three-tuple of strings representing this triple."""
         return StrTriple(self.subject.curie, self.predicate.curie, self.object.curie)
+
+    def as_uri_triple(self, converter: Converter) -> tuple[str, str, str]:
+        """Get a three-tuple of strings representing the expanded URIs."""
+        return (
+            converter.expand(self.subject, strict=True),
+            converter.expand(self.predicate, strict=True),
+            converter.expand(self.object, strict=True),
+        )
 
     def __lt__(self, other: Triple) -> bool:
         return self.as_str_triple() < other.as_str_triple()
@@ -106,3 +117,34 @@ def read_triples(path: str | Path, *, reference_cls: type[Reference] | None = No
             )
             for subject_curie, predicate_curie, object_curie in reader
         ]
+
+
+SEP = "\t"
+ENCODING = "utf8"
+
+
+def encode_triple(converter: Converter, triple: Triple) -> str:
+    """Encode a triple with URL-safe base64 encoding."""
+    return encode_delimited_uris(triple.as_uri_triple(converter))
+
+
+def encode_delimited_uris(uri_triple: tuple[str, str, str]) -> str:
+    """Encode a subject-predicate-object triple."""
+    delimited_uris = SEP.join(uri_triple)
+    return base64.urlsafe_b64encode(delimited_uris.encode(ENCODING)).decode(ENCODING)
+
+
+def decode_to_uris(s: str) -> tuple[str, str, str]:
+    """Decode a triple from URL-safe base64 encoding."""
+    delimited_uris = base64.urlsafe_b64decode(s.encode(ENCODING)).decode(ENCODING)
+    return cast(tuple[str, str, str], delimited_uris.split(SEP))
+
+
+def decode_triple(converter: Converter, s: str) -> Triple:
+    """Decode a triple from URL-safe base64 encoding."""
+    s, p, o = decode_to_uris(s)
+    return Triple(
+        subject=converter.parse_uri(s, strict=True).to_pydantic(),
+        predicate=converter.parse_uri(p, strict=True).to_pydantic(),
+        object=converter.parse_uri(o, strict=True).to_pydantic(),
+    )
