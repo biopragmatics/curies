404ab145270c3c734d1e1de2326c88a5f06b7b98
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-03-16 11:34:29 +0100

    Update triples.py

--- src/curies/triples.py
+++ src/curies/triples.py
@@ -1,4 +1,108 @@
-"""Utilities for triples."""
+"""Utilities for triples.
+
+Similarly to how the :mod:`curies` package enables the semantic representation of
+references (i.e., as CURIEs) with the :class:`curies.Reference` class, it enables the
+representation of semantic triples (i.e., as subject-predicate-object triples of CURIEs)
+with the :class:`curies.Triple` class.
+
+Triples can be constructed either from strings, which are implicitly parsed as CURIEs o
+
+.. code-block:: python
+
+    from curies import Triple, Reference
+
+    triple = Triple(
+        subject="mesh:C000089",
+        predicate="skos:exactMatch",
+        object="chebi:28646",
+    )
+
+    triple = Triple(
+        subject=Reference(prefix="mesh", identifier="C000089"),
+        predicate=Reference(prefix="skos", identifier="exactMatch"),
+        object=Reference(prefix="chebi", identifier="28646"),
+    )
+
+    from curies import NamableReference
+
+    triple = Triple(
+        subject=NamedReference(prefix="mesh", identifier="C000089", name="ammeline"),
+        predicate=NamableReference(prefix="skos", identifier="exactMatch"),
+        object=NamedReference(prefix="chebi", identifier="28646", name="ammeline"),
+    )
+
+
+    triple = Triple.from_uris(
+        converter=converter,
+        subject="http://example.org/1",
+        predicate="http://example.org/2",
+        object="http://example.org/3",
+    )
+
+###########################
+ Identification of Triples
+###########################
+
+The ``rdf`` namespace supports the explicit reification of triples. This means that an
+explicit identifier (typically, a blank node) can be used to refer to a triple itself,
+and the ``rdf:subject``, ``rdf:predicate`` and ``rdf:object`` predicates can be used to
+connect the identifier representing the triple to its respective subject, predicate, and
+object components.
+
+RDF enables explicit reification of triples with the following:
+
+.. code-block::
+
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ex: <http://example.org/> .
+
+    ex:1 ex:2 ex:3 .
+
+    [] rdf:type rdf:Statement ;
+        rdf:subject ex:1 ;
+        rdf:predicate ex:2 ;
+        rdf:object ex:3 .
+
+It would be nice to have an implementation-agnostic way of assigning an identifier to
+the triple!
+
+.. code-block::
+
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX ex: <http://example.org/> .
+    PREFIX triple: <https://w3id.org/triple/> .
+
+    ex:1 ex:2 ex:3 .
+
+    triple:aHR0cDovL2V4YW1wbGUub3JnLzEJaHR0cDovL2V4YW1wbGUub3JnLzIJaHR0cDovL2V4YW1wbGUub3JnLzM= rdf:type rdf:Statement ;
+        rdf:subject ex:1 ;
+        rdf:predicate ex:2 ;
+        rdf:object ex:3 .
+
+We introduce a deterministic, reversible way of hashing a triple to assign it an
+identifier:
+
+.. code-block:: python
+
+    import curies
+    from curies import Triple, Reference
+    from curies.triples import encode_triple
+
+    converter = curies.load_prefix_map(
+        {
+            "ex": "http://example.org/",
+        }
+    )
+
+    triple = Triple.from_uris(
+        converter=converter,
+        subject="http://example.org/1",
+        predicate="http://example.org/2",
+        object="http://example.org/3",
+    )
+
+    luid = encode_triple(converter, triple)
+"""
 
 from __future__ import annotations
 
@@ -34,7 +138,34 @@ class StrTriple(NamedTuple):
 
 
 class Triple(BaseModel):
-    """A model for a triple of subject-predicate-object triple."""
+    """A Pydantic model for a subject-predicate-object triple.
+
+    Triples can be constructed either from strings representing CURIEs or pre-parsed
+    :class:`Reference` objects representing CURIEs.
+
+    .. code-block:: python
+
+        from curies import Triple, Reference
+
+        # construction with string representations of CURIEs
+        triple = Triple(
+            subject="mesh:C000089",
+            predicate="skos:exactMatch",
+            object="chebi:28646",
+        )
+
+        # construction with object representations of CURIEs
+        triple = Triple(
+            subject=Reference(prefix="mesh", identifier="C000089"),
+            predicate=Reference(prefix="skos", identifier="exactMatch"),
+            object=Reference(prefix="chebi", identifier="28646"),
+        )
+
+    .. note::
+
+        It's up to you to validate your CURIEs are semantically sound, e.g., against the
+        :mod:`bioregistry`.
+    """
 
     model_config = ConfigDict(frozen=True)
 
@@ -74,14 +205,12 @@ class Triple(BaseModel):
         )
 
     @classmethod
-    def from_uris(
-        cls, subject_uri: str, predicate_uri: str, object_uri: str, converter: Converter
-    ) -> Self:
+    def from_uris(cls, subject: str, predicate: str, object: str, converter: Converter) -> Self:
         """Construct a triple from three URI strings."""
         return cls(
-            subject=converter.parse_uri(subject_uri, strict=True).to_pydantic(),
-            predicate=converter.parse_uri(predicate_uri, strict=True).to_pydantic(),
-            object=converter.parse_uri(object_uri, strict=True).to_pydantic(),
+            subject=converter.parse_uri(subject, strict=True).to_pydantic(),
+            predicate=converter.parse_uri(predicate, strict=True).to_pydantic(),
+            object=converter.parse_uri(object, strict=True).to_pydantic(),
         )
 
 
@@ -134,6 +263,8 @@ def read_triples(path: str | Path, *, reference_cls: type[Reference] | None = No
 
 SEP = "\t"
 ENCODING = "utf-8"
+TRIPLE_PREFIX = "triple"
+TRIPLE_URI_PREFIX = "https://w3id.org/triple/"
 
 
 def encode_triple(converter: Converter, triple: Triple) -> str:
@@ -157,8 +288,8 @@ def decode_triple(converter: Converter, xx: str) -> Triple:
     """Decode a triple from URL-safe base64 encoding."""
     subject_uri, predicate_uri, object_uri = decode_to_uris(xx)
     return Triple.from_uris(
-        subject_uri=subject_uri,
-        predicate_uri=predicate_uri,
-        object_uri=object_uri,
+        subject=subject_uri,
+        predicate=predicate_uri,
+        object=object_uri,
         converter=converter,
     )
