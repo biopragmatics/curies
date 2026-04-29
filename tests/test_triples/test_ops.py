a422333077838b7fd4a7bbb0132c875c4d3aad0e
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-04-29 14:33:38 +0200

    Update test_ops.py

--- tests/test_triples/test_ops.py
+++ tests/test_triples/test_ops.py
@@ -5,6 +5,7 @@ import unittest
 from curies import Reference
 from curies.triples import Triple
 from curies.triples.ops import (
+    exclude_prefix_stratified_many_to_many,
     flip_prefix_pair_stratified_index,
     get_one_to_many,
     get_prefix_pair_stratified_indexes,
@@ -64,7 +65,6 @@ class TestOperations(unittest.TestCase):
 
     def test_many_to_many_index(self) -> None:
         """Get getting a many-to-many index."""
-        self.maxDiff = None
         forward, backward = get_prefix_pair_stratified_indexes(triples)
         with self.subTest(part="contents"):
             self.assertEqual(
@@ -108,7 +108,6 @@ class TestOperations(unittest.TestCase):
                     },
                 },
                 get_one_to_many(forward),
-                msg="\nfailed on 1-n (forward)",
             )
             self.assertEqual(
                 {
@@ -119,7 +118,6 @@ class TestOperations(unittest.TestCase):
                     }
                 },
                 get_one_to_many(backward),
-                msg="\nfailed on n-1 (backward)",
             )
             self.assertEqual(
                 {
@@ -129,7 +127,49 @@ class TestOperations(unittest.TestCase):
                     },
                 },
                 flip_prefix_pair_stratified_index(get_one_to_many(backward)),
-                msg="\nfailed on n-1 (backward, flipped)",
+            )
+
+    def test_one_to_many(self) -> None:
+        """Get getting a many-to-many index."""
+        triples = [m1, m2, m3]
+        forward, backward = get_prefix_pair_stratified_indexes(triples)
+        with self.subTest(part="contents"):
+            self.assertEqual(
+                {
+                    ("p1", "p2"): {
+                        "1": {"A": [m1]},
+                        "2": {"B": [m2], "C": [m3]},
+                    },
+                },
+                forward,
+            )
+            self.assertEqual(
+                {
+                    ("p2", "p1"): {
+                        "A": {"1": [m1]},
+                        "B": {"2": [m2]},
+                        "C": {"2": [m3]},
+                    },
+                },
+                backward,
+            )
+
+        with self.subTest(part="processing"):
+            self.assertEqual(
+                {
+                    ("p1", "p2"): {
+                        "2": {"B": [m2], "C": [m3]},
+                    },
+                },
+                get_one_to_many(forward),
+            )
+            self.assertEqual(
+                {},
+                get_one_to_many(backward),
+            )
+            self.assertEqual(
+                {},
+                flip_prefix_pair_stratified_index(get_one_to_many(backward)),
             )
 
     def test_simple_indexes(self) -> None:
@@ -161,4 +201,9 @@ class TestOperations(unittest.TestCase):
 
     def test_get_many_to_many(self) -> None:
         """Test getting many-to-many triples."""
-        self.assertEqual({m6, m7, m8, m9}, get_prefix_stratified_many_to_many(triples))
+        many_to_many = {m6, m7, m8, m9}
+        self.assertEqual(many_to_many, get_prefix_stratified_many_to_many(triples))
+        self.assertEqual(
+            [m for m in triples if m not in many_to_many],
+            list(exclude_prefix_stratified_many_to_many(triples)),
+        )
